a4fd8834348cbdb58c5d9397ad9cdf7f2408a5a8
afeer123 <86306447+afeer123@users.noreply.github.com>
2022-05-07 19:23:10 +0800

    Add files via upload

---
+++ kernel-csgo/definitions.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "ntapi.hpp"
+
+#define CODE_CLIENT_REQUEST 0x1
+#define CODE_READ_MEMORY 0x2
+#define CODE_WRITE_MEMORY 0x3
+#define CODE_ENGINE_REQUEST 0x4
+
+typedef struct _INFO_STRUCT
+{
+	ULONG code;
+	ULONG process_id;
+	ULONG client_base;
+	ULONG engine_base;
+	ULONG address;
+	ULONG buffer;
+	ULONG size;
+}INFO_STRUCT, * PINFO_STRUCT;
